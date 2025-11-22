4d81fd614e877b30e7a512dd0ba5656567f98a3d
siwuagain <01simon.wu@gmail.com>
2025-11-22 12:44:26 -0500

    first draft, will test once we agree on a problem statement

--- problemskeleton_Problem32/input_format_validators/validate.py
+++ problemskeleton_Problem32/input_format_validators/validate.py
@@ -8,31 +8,45 @@
 import sys
 import re
 
+######
 n_line = sys.stdin.readline()
 print(repr(n_line)) # useful for debugging to see where we have read
 assert re.match('^[1-9][0-9]*\n$', n_line) # note: no leading zeros
 n = int(n_line)
-assert 1 <= n <= 100
+assert 1 <= n <= 100000
 
-unique_strings = set()
-for _ in range(n):
+prev_steps = prev_complexity = None
+for i in range(1, n+1):
     case_line = sys.stdin.readline()
     print(repr(case_line)) # useful for debugging to see where we have read
-    # check the line syntax (string, integer, real)
-    assert re.match('^[a-z]{1,20} (0|-?[1-9][0-9]{0,2}) (0|-?[1-9][0-9]?)(\.[0-9]{1,3})?\n$', case_line)
+    # check the line syntax (integer, integer)
+    assert re.match('^[1-9][0-9]* (0|[1-9][0-9]*)\n$', case_line)
 
     # parse the line
-    s, i, f = case_line.split()
-    assert -100 <= int(i) <= 100
-    assert -10.0 <= float(f) <= 10.0
+    s, c = case_line.split()
+    steps = int(s)
+    complexity = int(c)
 
-    # verify string uniqueness
-    assert s not in unique_strings
-    unique_strings.add(s)
+    #steps_1 should = 1
+    if i == 1:
+        assert steps == 1
+    
+    #steps_1 < steps_i < steps_n
+    if prev_steps is not None:
+        assert steps > prev_steps
+    
+    #complexity_1 > complexity_i > complexity_n
+    if prev_complexity is not None:
+        assert complexity < prev_complexity
+  
+    prev_steps = steps
+    prev_complexity = complexity
+
+#complexity_n should = 0
+assert prev_complexity == 0
 
 # ensure no extra input
 assert sys.stdin.readline() == ''
 
 # if we get here, all is well; use exit code 42.
 sys.exit(42)
-
