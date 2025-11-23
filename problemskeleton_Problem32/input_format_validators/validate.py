c636a392a111e52a69e2dfaf55cee2d215992ca3
siwuagain <01simon.wu@gmail.com>
2025-11-23 13:25:43 -0500

    added print statements for validator

--- problemskeleton_Problem32/input_format_validators/validate.py
+++ problemskeleton_Problem32/input_format_validators/validate.py
@@ -14,36 +14,50 @@ try:
     prev_steps = prev_complexity = None
     for i in range(1, n+1):
         case_line = sys.stdin.readline()
-        print(repr(case_line)) # useful for debugging to see where we have read
-        # check the line syntax (integer, integer)
-        assert re.match('^[1-9][0-9]* (0|[1-9][0-9]*)\n$', case_line)
-
+        
+        print(f"current line: {i + 1}")
+        print(f"Validating line format {repr(case_line)} against regex...")
+        assert re.match('^(0|[1-9][0-9]*) (0|[1-9][0-9]*)\n$', case_line)
+        print("Done validating line format against regex..")
         # parse the line
+
+        print("Parsing line values...")
         s, c = case_line.split()
         steps = int(s)
         complexity = int(c)
+        print(f"Done parsing line values... steps: {steps} complexity: {complexity}")
 
         #steps_1 should = 1
         if i == 1:
+            print("Validating first trap's step value")
             assert steps == 1
+            print("Done validating first trap's step value")
         
         #steps_1 < steps_i < steps_n
         if prev_steps is not None:
+            print(f"Validating current steps value {steps} is greater than the previous {prev_steps}...")
             assert steps > prev_steps
+            print("Steps value is valid")
         
         #complexity_1 > complexity_i > complexity_n
         if prev_complexity is not None:
+            print(f"Validating current complexity value {complexity} is lesser than the previous {prev_complexity}...")
             assert complexity < prev_complexity
+            print("Complexity value is valid")
     
         prev_steps = steps
         prev_complexity = complexity
 
     #complexity_n should = 0
+    print(f"Validating last complexity's value {prev_complexity}")
     assert prev_complexity == 0
+    print("Done validating last complexity's value")
 
+    print("Validating no input left...")
     # ensure no extra input
     assert sys.stdin.readline() == ''
-
+    print("Input file is valid, exiting successfully...")
+    
     # if we get here, all is well; use exit code 42.
     sys.exit(42)
 except:
